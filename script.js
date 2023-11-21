d4ea3d8b3cae35ff943eb481f1b595789136fdbf
klaudix3232 <klaudix121314@gmail.com>
2023-11-21 13:26:17 +0000

    added js script to manipulate with navbar style

---
+++ script.js
@@ -0,0 +1,18 @@
+let navbarExpanded = false;
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('navbar-toggler');
+
+    button.onclick = function(event) {
+
+        const nav = document.querySelector('nav');
+        if (navbarExpanded) {
+            nav.style.backdropFilter = 'blur(0px)';
+            navbarExpanded = false;
+        } else {
+            nav.style.backdropFilter = 'blur(100px)';
+            navbarExpanded = true;
+        }
+    };
+  });
+
