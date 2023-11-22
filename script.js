bf98572627ca2a07084de3a4b790a9f04587114e
klaudix3232 <klaudix121314@gmail.com>
2023-11-22 12:32:02 +0000

    js fixed and set backdrop filter blur= 0 px after imediate loaded the DOM

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-let navbarExpanded = false;
+// let navbarExpanded = false;
 
-document.addEventListener('DOMContentLoaded', (event) => {
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const button = document.getElementById('navbar-toggler');
+
+//     button.onclick = function(event) {
+
+//         const nav = document.querySelector('nav');
+//         if (navbarExpanded) {
+//             nav.style.backdropFilter = 'blur(0px)';
+//             nav.style.webkitBackdropFilter =  'blur(0px)';
+//             navbarExpanded = false;
+//         } else {
+//             nav.style.backdropFilter = 'blur(100px)';
+//             nav.style.webkitBackdropFilter =  'blur(100px)';
+//             navbarExpanded = true;
+//         }
+//     };
+//   });
+
+  let navbarExpanded = false;
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the navbar and button elements
+    const nav = document.querySelector('nav');
     const button = document.getElementById('navbar-toggler');
-
+  
+    // Set the initial backdropFilter style for the navbar
+    nav.style.backdropFilter = 'blur(0px)';
+    nav.style.webkitBackdropFilter = 'blur(0px)'; // For Safari
+  
+    // Set up the button click event
     button.onclick = function(event) {
-
-        const nav = document.querySelector('nav');
-        if (navbarExpanded) {
-            nav.style.backdropFilter = 'blur(0px)';
-            nav.style.webkitBackdropFilter =  'blur(0px)';
-            navbarExpanded = false;
-        } else {
-            nav.style.backdropFilter = 'blur(100px)';
-            nav.style.webkitBackdropFilter =  'blur(100px)';
-            navbarExpanded = true;
-        }
+      if (navbarExpanded) {
+        nav.style.backdropFilter = 'blur(0px)';
+        nav.style.webkitBackdropFilter = 'blur(0px)';
+        navbarExpanded = false;
+      } else {
+        nav.style.backdropFilter = 'blur(100px)';
+        nav.style.webkitBackdropFilter = 'blur(100px)';
+        navbarExpanded = true;
+      }
     };
   });
-
+  
